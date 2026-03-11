ebe72f1b8f41fb1b01e2f780f7f5955841a440ee
Gyuseriyoung <gyuseriyoung@naver.com>
2026-03-11 16:28:04 +0900

    상하좌우 캐릭터

---
+++ 3D/Test 2/Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework.Constraints;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private Vector2 moveInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float movespeed = 12.5f;
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveInput.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if(moveInput.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (moveInput.y > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput.y < 0)
+        {
+            transform.localScale = new Vector3(1, -1, 1);
+        }
+        transform.Translate(Vector3.right * moveInput.x * Time.deltaTime * movespeed);
+        transform.Translate(Vector3.up * moveInput.y * Time.deltaTime * movespeed);
+    }
+}
