a83e2820ed65dddc4f1f826cd3e7da8d1c446f00
Gyuseriyoung <gyuseriyoung@naver.com>
2026-03-18 16:11:35 +0900

    [Feat]JumpMap 점프, 맵, 애니메이션, 트리거를 이용한 씬 변경 추가

--- 3D/JumpMap/Assets/NewMonoBehaviourScript.cs
+++ 3D/JumpMap/Assets/NewMonoBehaviourScript.cs
@@ -1,18 +1,41 @@
 using NUnit.Framework.Constraints;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using UnityEngine.SceneManagement;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     private Vector2 moveInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    public float movespeed = 12.5f;
+    public float movespeed = 6.5f;
+    public float jumpforce = 3f;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("PlayScene_" + collision.name);
 
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();  
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
+
+    public void OnJump (InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpforce);
+        }
+    }
+
+    
 
     // Update is called once per frame
     void Update()
@@ -27,5 +50,13 @@ public class NewMonoBehaviourScript : MonoBehaviour
         }
         transform.Translate(Vector3.right * moveInput.x * Time.deltaTime * movespeed);
         transform.Translate(Vector3.up * moveInput.y * Time.deltaTime * movespeed);
+
+        if (moveInput.magnitude > 0)
+        {
+            myAnimator.SetBool("move", true);
+        }
+        else {
+            myAnimator.SetBool("move", false);
+        }
     }
 }
