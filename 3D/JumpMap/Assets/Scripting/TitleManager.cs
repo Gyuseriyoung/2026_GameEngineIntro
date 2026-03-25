d35fc42bf9f444a904cf80df8abe56d88228f8f2
Gyuseriyoung <gyuseriyoung@naver.com>
2026-03-25 16:29:46 +0900

    [Feat] 점프 게임 타이틀

---
+++ 3D/JumpMap/Assets/Scripting/TitleManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject helpPanel; 
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("PlayScene_Door1");
+    }
+   public void HelpOpen()
+    {
+        helpPanel.SetActive(true);
+    }
+
+    public void HelpClose()
+    {
+        helpPanel.SetActive(false);
+    }
+
+    public void GameQuit()
+    {
+        Application.Quit();
+    }
+}
